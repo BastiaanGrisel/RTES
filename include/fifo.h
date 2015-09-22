37de8c4436e5aff28f4ac0da9748b00aac2367fa
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-09-22 20:37:12 +0200

    Added comments to fifo

--- include/fifo.h
+++ include/fifo.h
@@ -1,21 +1,24 @@
+/* Author Bastiaan */
 #include <stdio.h>
 #include "types.h"
 
 #define FIFO_CAPACITY 128 // Can store 127 values!
 
+/* Structure to hold fifo so multiple ones can be made */
 typedef struct {
 	int in;
 	int out;
 	char elements[128];
 } Fifo;
 
+/* Call this function beofre using the fifo */
 void fifo_init(Fifo *fifo)
 {
 	fifo->in = 0;
 	fifo->out = 0;
 }
 
-// Add char to queue
+/* Append a character to the fifo as long as it is not full */
 bool fifo_put(Fifo* fifo, char c)
 {
 	if(fifo_size(fifo) == FIFO_CAPACITY - 1)
@@ -27,7 +30,7 @@ bool fifo_put(Fifo* fifo, char c)
 	return true;
 }
 
-// pop
+/* Get a character from the fifo at position n */
 int fifo_get(Fifo* fifo, char *old, bool remove, int n)
 {
 	if(fifo_size(fifo) == 0)
@@ -43,20 +46,22 @@ int fifo_get(Fifo* fifo, char *old, bool remove, int n)
 	return true;
 }
 
-// peek at first char
+/* Get the first character in the fifo (without removing it) */
 bool fifo_peek(Fifo* fifo, char *c) {
 	return fifo_get(fifo, c, false, 0);
 }
 
+/* Get the n-th character of the fifo (without removing it) */
 bool fifo_peek_at(Fifo* fifo, char *c, int n) {
 	return fifo_get(fifo, c, false, n);
 }
 
-// pop first char
+/* Remove the first character in the fifo */
 bool fifo_pop(Fifo* fifo, char *c) {
 	return fifo_get(fifo, c, true, 0);
 }
 
+/* Get the size of the fifo */
 int fifo_size(Fifo* fifo) {
 	if(fifo->in > fifo->out)
 		return fifo->in - fifo->out;
