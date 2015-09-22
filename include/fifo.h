17707e9f874e357b6cf5c9ace5e972646c2e7b88
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-09-22 11:30:13 +0200

    Fifo with struct

--- include/fifo.h
+++ include/fifo.h
@@ -1,63 +1,71 @@
 #include <stdio.h>
 #include "types.h"
 
-#define QUEUE_ELEMENTS 100
+#define FIFO_CAPACITY 127
+
+typedef struct {
+	int in;
+	int out;
+	char elements[127];
+} Fifo;
+
+
 #define QUEUE_SIZE (QUEUE_ELEMENTS + 1)
-char fifo_[QUEUE_ELEMENTS];
 int fifo_in, fifo_out;
 
-void fifo_init(void)
+void fifo_init(Fifo *fifo)
 {
-	fifo_in = fifo_out = 0;
+	fifo->in = 0;
+	fifo->out = 0;
 }
 
 // Add char to queue
-bool fifo_put(char c)
+bool fifo_put(Fifo* fifo, char c)
 {
-	if(fifo_size() == QUEUE_ELEMENTS)
+	if(fifo_size() == FIFO_CAPACITY)
 		return false;
 
-	fifo_[fifo_in++] = c;
+	fifo->elements[fifo->in++] = c;
 	return true;
 }
 
 // pop
-int fifo_get(char *old, bool remove, int n)
+int fifo_get(Fifo* fifo, char *old, bool remove, int n)
 {
 	if(fifo_size() == 0)
 		return false;
 
-	*old = fifo_[((fifo_out + n) % QUEUE_ELEMENTS)];
+	*old = fifo->elements[((fifo->out + n) % FIFO_CAPACITY)];
 
 	if(remove)
-		fifo_out++;
+		fifo->out++;
 
 	return true;
 }
 
 // peek at first char
-char fifo_peek() {
+char fifo_peek(Fifo* fifo) {
 	char c;
-	fifo_get(&c, false, 0);
+	fifo_get(fifo, &c, false, 0);
 	return c;
 }
 
-char fifo_peek_at(int n) {
+char fifo_peek_at(Fifo* fifo, int n) {
 	char c;
-	fifo_get(&c, false, n);
+	fifo_get(fifo, &c, false, n);
 	return c;
 }
 
 // pop first char
-char fifo_pop() {
+char fifo_pop(Fifo* fifo) {
 	char c;
-	fifo_get(&c, true, 0);
+	fifo_get(fifo, &c, true, 0);
 	return c;
 }
 
-int fifo_size() {
-	if(fifo_in > fifo_out)
-		return fifo_in - fifo_out;
+int fifo_size(Fifo* fifo) {
+	if(fifo->in > fifo->out)
+		return fifo->in - fifo->out;
 	else
-		return QUEUE_SIZE + fifo_in - fifo_out;
+		return FIFO_CAPACITY + fifo->in - fifo->out;
 }
--- include/fifo_test.c
+++ include/fifo_test.c
@@ -3,58 +3,60 @@
 #include <stdio.h>
 
 int main() {
-	fifo_init();
+	Fifo fifo;
+	fifo_init(&fifo);
+
 	char c;
 	int i;	
 	
 	// put a char
-	fifo_put('a');
-	printf("Pop a: %c\n",fifo_pop());
+	fifo_put(&fifo, 'a');
+	printf("Pop a: %c\n",fifo_pop(&fifo));
 
 	// Print the size
-	fifo_put('a');
-	printf("Size (1): %i\n", fifo_size()); 
-	fifo_pop();
+	fifo_put(&fifo, 'a');
+	printf("Size (1): %i\n", fifo_size(&fifo)); 
+	fifo_pop(&fifo);
 	
 	// put several chars
-	fifo_put('a');
-	fifo_put('b');
-	fifo_put('c');
-	printf("Size (3): %i\n", fifo_size());
-	printf("Pop a: %c\n",fifo_pop());
-	printf("Pop b: %c\n",fifo_pop());
-	printf("Pop b: %c\n",fifo_pop());
-
+	fifo_put(&fifo, 'a');
+	fifo_put(&fifo, 'b');
+	fifo_put(&fifo, 'c');
+	printf("Size (3): %i\n", fifo_size(&fifo));
+	printf("Pop a: %c\n",fifo_pop(&fifo));
+	printf("Pop b: %c\n",fifo_pop(&fifo));
+	printf("Pop b: %c\n",fifo_pop(&fifo));
+	
 	// Print int 1
-	fifo_put(1);
-	printf("Pop 1: %i\n",fifo_pop());
+	fifo_put(&fifo, 1);
+	printf("Pop 1: %i\n",fifo_pop(&fifo));
 
 	// put negative number
-	fifo_put(-127);
-	printf("Pop -127: %i\n",fifo_pop());
+	fifo_put(&fifo, -127);
+	printf("Pop -127: %i\n",fifo_pop(&fifo));
 	
 	// Check peek
-	fifo_put(34);
-	printf("Peek 34: %i\n",fifo_peek());
-	printf("Size 1: %i\n", fifo_size());
-	fifo_pop();
+	fifo_put(&fifo, 34);
+	printf("Peek 34: %i\n",fifo_peek(&fifo));
+	printf("Size 1: %i\n", fifo_size(&fifo));
+	fifo_pop(&fifo);
 	
 	// Check peek 2
-	fifo_put(75);
-	fifo_put(32);
-	printf("Peek 32: %i\n",fifo_peek_at(1));
-	printf("Size 2: %i\n", fifo_size());
+	fifo_put(&fifo, 75);
+	fifo_put(&fifo, 32);
+	printf("Peek 32: %i\n",fifo_peek_at(&fifo, 1));
+	printf("Size 2: %i\n", fifo_size(&fifo));
 
 	// overflowtest (does not add any elements)
 	int j;
 	for(j = 0; j < 110; j++) {
-		fifo_put(j);
+		fifo_put(&fifo, j);
 	}	
 
-	printf("Size (100): %i\n", fifo_size());
-	printf("Pop 75: %i\n", fifo_pop());	
-	printf("Pop 32: %i\n", fifo_pop());	
-
+	printf("Size (100): %i\n", fifo_size(&fifo));
+	printf("Pop 75: %i\n", fifo_pop(&fifo));	
+	printf("Pop 32: %i\n", fifo_pop(&fifo));	
+	
 	return 1;
 }
 
