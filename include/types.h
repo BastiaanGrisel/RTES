65bbc4a9a56f7ad40d41b8519b1ddd02fd977867
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-09-26 14:02:08 +0200

    Added extra int types

--- include/types.h
+++ include/types.h
@@ -10,5 +10,13 @@ typedef enum { NONE, SENSORS } Loglevel;
 #define UP_CHAR 't'
 #define DOWN_CHAR 'g'
 
+typedef signed char       int8_t;
+typedef signed short      int16_t;
+typedef signed int        int32_t;
+typedef unsigned char     uint8_t;
+typedef unsigned short    uint16_t;
+typedef unsigned int      uint32_t;
+
+
 #endif /* TYPES_H */
 
