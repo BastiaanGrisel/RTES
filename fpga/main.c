b676b6fc3a8d2bc969eb52360d93c870777d6f7a
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-10-06 11:52:59 +0200

    Message queue check in separate header file

--- fpga/main.c
+++ fpga/main.c
@@ -405,36 +405,6 @@ void check_alive_connection() {
 		set_mode(PANIC);
 }
 
-void check_msg_q(Fifo *q, void (*callback)(char, PacketData), void (*error)()){
-	while(fifo_size(q) >= 4) { // Check if there are one or more packets in the queue
-		char control;
-		PacketData data;
-		char checksum;
-
-		fifo_peek_at(q, &control, 0);
-		fifo_peek_at(q, &data.as_bytes[0], 1);
-		fifo_peek_at(q, &data.as_bytes[1], 2);
-		fifo_peek_at(q, &checksum, 3);
-
-		if(!check_packet(control,data,checksum)) {
-			// If the checksum is not correct, pop the first message off the queue and repeat the loop
-			char c;
-			fifo_pop(q, &c);
-
-			error();
-		} else {
-			// If the checksum is correct, pop the packet off the queue and notify a callback
-			char c;
-			fifo_pop(q, &c);
-			fifo_pop(q, &c);
-			fifo_pop(q, &c);
-			fifo_pop(q, &c);
-
-			callback(control, data);
-		}
-	}
-}
-
 int32_t main(void)
 {
 	setup();
@@ -449,7 +419,7 @@ int32_t main(void)
 
 		// Process messages
         	DISABLE_INTERRUPT(INTERRUPT_PRIMARY_RX); // Disable incoming messages while working with the message queue
-		check_msg_q(&pc_msg_q, &packet_received, &lost_packet);
+		check_for_new_packets(&pc_msg_q, &packet_received, &lost_packet);
 		ENABLE_INTERRUPT(INTERRUPT_PRIMARY_RX); // Re-enable messages from the PC after processing them
 	}
 
