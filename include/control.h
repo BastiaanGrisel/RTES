0223f6d82a5499301e3a2f7e671511b2a07ed632
Synchro <alessio.salman@gmail.com>
2015-10-09 19:24:48 +0200

    macro defined in control.h

---
+++ include/control.h
@@ -0,0 +1,16 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+/*Here go all the control functions*/
+
+//TODO
+/*define this constants if we need predefined shifting functions
+#define L_PRECISION 1
+#define  R_PRECISION 1 */
+//#define BITSHIFT(num) ( (SHIFT) > 0 ? ( (num) >> (SHIFT) ) : ( (num) << -1*(SHIFT) ) )
+
+#define RSHIFT(num,shift) ( (num) >> (shift) )
+#define LSHIFT(num,shift) ( (num) << (shift) )
+#define INCREASE_PRECISION(num,shift) ( (num) << (shift) )
+#define DECREASE_PRECISION(num,shift) ( (num) >> (shift) )
+
+#endif
