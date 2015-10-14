cb367da7d6944e37c69c3b53426c24acbdbae98b
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-10-15 00:39:53 +0200

    Tried to add colors

--- pc/keysandjs.c
+++ pc/keysandjs.c
@@ -54,15 +54,13 @@ int main (int argc, char **argv)
 
 
 	start_color();
+	init_pair(0, COLOR_WHITE, COLOR_BLACK);
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 	init_pair(2, COLOR_GREEN, COLOR_BLACK);
-	init_pair(3, COLOR_BLUE, COLOR_BLACK);
-	init_pair(4, COLOR_BLACK, COLOR_GREEN);
-	init_pair(5, COLOR_BLACK, COLOR_CYAN);
-	init_pair(6, COLOR_BLACK, COLOR_YELLOW);
-	init_pair(7, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(8, COLOR_RED,COLOR_YELLOW);
-	init_pair(9, COLOR_BLACK,COLOR_WHITE);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(4, COLOR_BLUE, COLOR_BLACK);
+	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(6, COLOR_CYAN, COLOR_BLACK);
 
 	gettimeofday(&time,NULL);
 	gettimeofday(&keep_alive,NULL);
